8b98d0420d1f1154a39db4196f068b7952b71fb1
Nuha  Bin Tayyash <nuhabintayyash@Nuha-778.local>
2021-01-20 23:41:31 +0300

    Comparing with tradeSeq

---
+++ demo_notebooks/sparse_version.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+from matplotlib import pyplot as plt
+import statsmodels.api as sm
+import numpy as np
+import matplotlib.lines as mlines
+import gpflow
+
+def plot(params,X,Y,LLR = None,sparse = False):
+    
+    indexes = Y.index.values # list of genes to be plotted 
+    xtest = np.linspace(np.min(X)-.1,np.max(X)+.1,100)[:,None] # points to make prediction
+    #likelihood = params['likelihood']
+    for i in range(len(indexes)):
+        fig = plt.figure()
+        print(indexes[i])
+        model_index = 1
+        y = Y.iloc[i].values # genes expression data
+        
+        if params['likelihood'] == 'Gaussian':
+                y = np.log(y+1)
+        
+        for mean,var,model in zip(params['means'][i],params['vars'][i],params['models'][i]):
+            gpflow.utilities.print_summary(model, fmt='notebook')
+            plt.tick_params(labelsize='large', width=2)     
+            plt.ylabel('Gene Expression', fontsize=16)
+            plt.xlabel('Times', fontsize=16)
+            c = 'royalblue'
+
+            if model_index == 3:
+                c = 'green'
+
+            plt.plot(xtest, mean,color= c, lw=2) 
+
+            if params['likelihood'] == 'Gaussian':
+                plt.fill_between(xtest[:,0],
+                                    mean[:,0] - 1*np.sqrt(var[:,0]),
+                                    mean[:,0] + 1*np.sqrt(var[:,0]),color=c,alpha=0.2) # one standard deviation
+                plt.fill_between(xtest[:,0],
+                                    mean[:,0] - 2*np.sqrt(var[:,0]),
+                                    mean[:,0] + 2*np.sqrt(var[:,0]),color=c, alpha=0.1)# two standard deviation
+            else:
+
+                lowess = sm.nonparametric.lowess    
+                # one standard deviation 68%
+                percentile_16 = lowess(np.percentile(var, 16, axis=0),xtest[:,0],frac=1./5, return_sorted=False)
+                #percentile_16 = np.percentile(var, 16, axis=0)
+                percentile_16 = [(i > 0) * i for i in percentile_16]
+                percentile_84 = lowess(np.percentile(var, 84, axis=0),xtest[:,0],frac=1./5, return_sorted=False)
+                #percentile_84 = np.percentile(var, 84, axis=0)
+                percentile_84 = [(i > 0) * i for i in percentile_84]
+                plt.fill_between(xtest[:,0],percentile_16,percentile_84,color=c,alpha=0.2)
+
+                # two standard deviation 95%
+                percentile_5 = lowess(np.percentile(var, 5, axis=0),xtest[:,0],frac=1./5, return_sorted=False)
+                #percentile_5 = np.percentile(var, 5, axis=0)
+                percentile_5 = [(i > 0) * i for i in percentile_5]
+                percentile_95 = lowess(np.percentile(var,95, axis=0),xtest[:,0],frac=1./5, return_sorted=False)
+                #percentile_95 = np.percentile(var,95, axis=0)
+                percentile_95 = [(i > 0) * i for i in percentile_95]
+                plt.fill_between(xtest[:,0],percentile_5,percentile_95,color=c,alpha=0.1)
+
+            if params['test_name'] == 'Two_samples_test':
+                if model_index == 1:
+                    if LLR is not None:
+                        label = LLR.loc[indexes[[i]]]['log_likelihood_ratio'].values[0]
+                        plt.scatter(X[0:int(X.shape[0]/2)],y[0:int(X.shape[0]/2)], s=30, marker='o', color= 'royalblue',alpha=1.,label = str(indexes[i])+'-LLR='+'{00:.2f}'.format(label)) #data               
+                        plt.legend(loc='best') 
+                    else:
+                        plt.scatter(X[0:int(X.shape[0]/2)],y[0:int(X.shape[0]/2)], s=30, marker='o', color= 'royalblue',alpha=1.) #data               
+                       
+                    plt.scatter(X[int(X.shape[0]/2)::],y[int(X.shape[0]/2)::], s=30, marker='o', color= 'green',alpha=1.) #data
+                    
+                elif model_index == 2:
+                    plt.scatter(X[0:int(X.shape[0]/2)],y[0:int(X.shape[0]/2)], s=30, marker='o', color= 'royalblue',alpha=1.) #data
+                    
+                else:
+                    plt.scatter(X[int(X.shape[0]/2)::],y[int(X.shape[0]/2)::], s=30, marker='o', color= 'green',alpha=1.) #data
+                    
+           
+            else: 
+                if LLR is not None:
+                    if params['test_name'] == 'One_sample_test':
+                        label =  str(indexes[i])+'-LLR ='+'{00:.2f}'.format(LLR.loc[indexes[[i]]]['log_likelihood_ratio'].values[0])
+                        
+                    else:
+                        label = str(indexes[i])+'-LL ='+'{00:.2f}'.format(LLR.loc[indexes[[i]]]['Dynamic_model_log_likelihood'].values[0])
+                        
+                    plt.scatter(X,y,s=30,marker = 'o',color=c,alpha=1.,label = label)  
+                else:
+                    plt.scatter(X,y,s=30,marker = 'o',color=c,alpha=1.) 
+                
+            if model_index == 1 and LLR is not None:
+                plt.legend(loc='best') 
+                
+            if sparse:
+                inducing_points = model.inducing_variable.Z.numpy() 
+                # discard any inducing points outside X range
+                inducing_points = inducing_points[inducing_points >= np.min(X)]
+                inducing_points = inducing_points[inducing_points <= np.max(X)]
+                plt.scatter(inducing_points,np.zeros(inducing_points.shape[0]),s=30,marker = '^',color='red',label='inducing points',alpha=1.) 
+                #plt.legend(loc='upper center', bbox_to_anchor=(1.20, 0.1))
+            
+            if not(params['test_name'] == 'Two_samples_test' and model_index == 2):
+                plt.show()
+            
+            model_index = model_index + 1
+
+def plotGene(ax, X, Y, label, colors=['b', 'r'], size=10, alpha=0.6):
+    cellColors = [colors[int(i) - 1] for i in label]
+    ax.scatter(X, Y, s=size, c=cellColors, alpha=alpha)
+
+def plotBranching(d):
+    plt.rcParams['xtick.labelsize'] = 12
+    plt.rcParams['ytick.labelsize'] = 12
+
+    lowess = sm.nonparametric.lowess
+
+    fig, ax = plt.subplots(2, 1, figsize=(10, 8), gridspec_kw={'height_ratios': [4, 3]})
+
+    mu = d['mean']
+    var = d['variance']
+    Xnew = d['Xnew']
+    testTimes = d['test_times']
+    pt = d['MAP_model'].data[0]
+
+    lower_lim = len(Xnew)
+    upper_lim = 2 * lower_lim
+
+    if d['likelihood'] == 'Gaussian':
+        geneExpressionTitle = 'log(counts + 1)'
+        upper = mu + 2 * np.sqrt(var)
+        lower = mu - 2 * np.sqrt(var)
+        ax[0].plot(Xnew[:, 0], mu[0:lower_lim, :], '-', lw=3, color='b')
+        ax[0].fill_between(Xnew[:, 0], lower[0:lower_lim, :].numpy().reshape(-1), upper[0:lower_lim, :].numpy().reshape(-1),
+                           color='blue', alpha=0.2)
+        ax[0].plot(Xnew[:, 0], mu[lower_lim:upper_lim, :], '-', lw=3, color='r')
+        ax[0].fill_between(Xnew[:, 0], lower[lower_lim:upper_lim, :].numpy().reshape(-1), upper[lower_lim:upper_lim, :].numpy().reshape(-1),
+                           color='r', alpha=0.2)
+
+        plotGene(ax[0], pt[:, 0], np.log(d['MAP_model'].data[1] + 1), pt[:, 1], size=40, alpha=.6)
+
+    else:
+        geneExpressionTitle = 'Counts'
+        num_time_points = upper_lim
+        percentile_5 = np.asarray(np.percentile(var, 5, axis=0)).reshape([num_time_points, 1])
+        percentile_95 = np.asarray(np.percentile(var, 95, axis=0)).reshape([num_time_points, 1])
+
+        ax[0].plot(Xnew[:, 0], mu[0:lower_lim], '-', lw=3, color='b')
+        ax[0].fill_between(Xnew[:, 0], lowess(percentile_5[0:lower_lim, 0], Xnew[:, 0], frac=1. / 5, return_sorted=False),
+                           lowess(percentile_95[0:lower_lim, 0], Xnew[:, 0], frac=1. / 5, return_sorted=False), color='blue',
+                           alpha=0.2)
+        ax[0].plot(Xnew, mu[lower_lim:upper_lim], '-', lw=3, color='r')
+        ax[0].fill_between(Xnew[:, 0], lowess(percentile_5[lower_lim:upper_lim, 0], Xnew[:, 0], frac=1. / 5, return_sorted=False),
+                           lowess(percentile_95[lower_lim:upper_lim, 0], Xnew[:, 0], frac=1. / 5, return_sorted=False), color='r',
+                           alpha=0.2)
+        plotGene(ax[0], pt[:, 0], d['MAP_model'].data[1], pt[:, 1], size=40, alpha=.6)
+
+    ax[0].set_ylabel(geneExpressionTitle, fontsize=16)
+
+    blue_line = mlines.Line2D([], [], color='blue', linewidth=3., label='Predicted Mean (branch 1)')
+    red_line = mlines.Line2D([], [], color='red', linestyle='-', linewidth=3., label='Predicted Mean (branch 1)')
+    blue_dot = mlines.Line2D([], [], color='blue', marker='o', markersize='8', linestyle='', label='branch 1')
+    red_dot = mlines.Line2D([], [], color='red', marker='o', markersize='8', linestyle='', label='branch 2')
+    ax[0].legend(handles=[blue_line, red_line, blue_dot, red_dot], bbox_to_anchor=(1.25, 0.8), loc=10, fontsize=14,
+                 frameon=True)
+    #     ax[0].legend(handles=[blue_line, red_line, blue_dot, red_dot], loc=2, fontsize=12)
+
+    width = testTimes[1] - testTimes[0]
+    ax[1].bar(testTimes, d['branching_probability'], color='royalblue', align='center', edgecolor="white", width=width)
+    ax[1].set_xlabel('Pseudotime', fontsize=16)
+    ax[1].set_ylabel('Branching probability', fontsize=16)
+    plt.subplots_adjust(wspace=0, hspace=0)
+    return fig, ax
---
+++ paper_notebooks/tradeSeq/performanceTIPlotCyclic_acrossSimulations.R
@@ -0,0 +1,252 @@
+setwd("/Users/nuhabintayyash/GPcounts/paper_notebooks/tradeSeq/")
+library(here)
+library(slingshot)
+library(RColorBrewer)
+library(mgcv)
+library(tradeSeq)
+library(edgeR)
+library(rafalib)
+library(wesanderson)
+library(ggplot2)
+library(cowplot)
+library(iCOBRA)
+library(scales)
+library(dplyr)
+
+
+### prepare performance plots ####
+cols <- c(rep(c("#C6DBEF", "#08306B"), each = 3), "#4292C6", "#4daf4a",
+          "#e41a1c", "#e78ac3", "#ff7f00", "darkgoldenrod1")
+names(cols) <- c("tradeSeq_slingshot_end", "tradeSeq_GPfates_end", "tradeSeq_true_3k",
+                 "tradeSeq_slingshot_pattern", "tradeSeq_GPfates_pattern",
+                 "tradeSeq_true_5k", "tradeSeq_slingshot_assoc", "GPcounts_NB",
+                 "tradeSeq_true_10k", "GPcounts_true_Gaussian", "GPcounts_Gaussian", "GPcounts_true_NB")
+linetypes <- c(rep(c("solid", "solid", "solid"), 2), rep("solid", 7))
+names(linetypes) <- c("tradeSeq_slingshot_end", "tradeSeq_GPfates_end", "tradeSeq_true_3k",
+                      "tradeSeq_slingshot_pattern", "tradeSeq_GPfates_pattern",
+                      "tradeSeq_true_5k", "tradeSeq_slingshot_assoc", "GPcounts_NB",
+                      "tradeSeq_true_10k", "GPcounts_true_Gaussian", "GPcounts_Gaussian", "GPcounts_true_NB")
+theme_set(theme_bw())
+theme_update(legend.position = "none",
+             panel.grid.major.y = element_blank(),
+             panel.grid.minor.y = element_blank(),
+             panel.grid.major.x = element_line(linetype = "dashed", colour = "black"),
+             panel.grid.minor.x = element_line(linetype = "dashed", colour = "grey"),
+             axis.title.x = element_text(size = rel(1)),
+             axis.title.y = element_text(size = rel(1)),
+             axis.text.x = element_text(size = rel(.8)),
+             axis.text.y = element_text(size = rel(.8)))
+
+dir <-("/Users/nuhabintayyash/GPcounts/paper_notebooks/tradeSeq/")
+
+cobraFiles <- list.files(dir, pattern="cobra*", full.names=TRUE)
+cobraFiles <- cobraFiles[c(1,3:10,2)] #order from 1 to 10
+
+plotPerformanceCurve <- function(cobraObject){
+  cn <- colnames(pval(cobraObject))
+  cn <- gsub(cn,pattern="tradeR",replacement="tradeSeq")
+  
+  colnames(pval(cobraObject)) <- cn
+  colnames(cobraObject@pval) <- gsub(colnames(cobraObject@pval),pattern="tradeR",replacement="tradeSeq")
+
+  cobraObject <- calculate_adjp(cobraObject)
+  cobraObject <- calculate_performance(cobraObject, binary_truth = "status")
+
+  DyntoyPlot <- data.frame(FDP = cobraObject@fdrtprcurve$FDR,
+                           TPR = cobraObject@fdrtprcurve$TPR,
+                           method = cobraObject@fdrtprcurve$method,
+                         cutoff = cobraObject@fdrtprcurve$CUTOFF)
+  pDyntoy <- ggplot(DyntoyPlot, aes(x = FDP, y = TPR, col = method)) +
+    geom_path(size = 1, aes(linetype = method)) +
+    xlab("FDP") +
+    scale_x_continuous(limits = c(0, 1), breaks = c(0.01, 0.05, 0.1, 0.5, 1),
+                       minor_breaks = c(0:5) * .1) +
+    scale_y_continuous(limits = c(0, 1)) +
+    scale_color_manual(values = cols, breaks = names(cols)) +
+    scale_linetype_manual(values = linetypes, breaks = names(linetypes))
+    pDyntoy
+}
+
+for(ii in 1:length(cobraFiles)){
+    cobra <- readRDS(cobraFiles[ii])
+    assign(paste0("bifplot",ii),plotPerformanceCurve(cobra))
+}
+
+p1 <- plot_grid(bifplot1, bifplot2, bifplot3, bifplot4, bifplot5,
+           bifplot6, bifplot7, bifplot8, bifplot9, bifplot10,
+        nrow=2, ncol=5)
+p1
+
+plotsBif <- sapply(cobraFiles, function(file){
+  cobra <- readRDS(file)
+  plotPerformanceCurve(cobra)
+})
+resPlots <- plotsBif[seq(1,length(plotsBif),by=9)] # get relevant data frame
+
+
+pAll <- ggplot(resPlots[[1]],
+               aes(x = FDP, y = TPR, col = method)) +
+  geom_path(size = 1, aes(linetype = method)) +
+  scale_color_manual(values = cols, breaks = names(cols)) +
+  scale_linetype_manual(values = linetypes, breaks = names(linetypes))  + guides(fill=guide_legend(nrow=3))
+
+legend_all <- get_legend(pAll + labs(col = "", linetype = "") +
+                           theme(legend.position = "bottom",
+                                 legend.key.width = unit(1.3, "cm")))
+
+pLeg <- plot_grid(pAll, legend_all, rel_heights=c(1,0.2), nrow=2, ncol=1)
+pLeg
+
+#### plot with trajectories
+  FQnorm <- function(counts){
+    rk <- apply(counts,2,rank,ties.method='min')
+    counts.sort <- apply(counts,2,sort)
+    refdist <- apply(counts.sort,1,median)
+    norm <- apply(rk,2,function(r){ refdist[r] })
+    rownames(norm) <- rownames(counts)
+    return(norm)
+  }
+pal <- wes_palette("Zissou1", 12, type = "continuous")
+dataAll <- readRDS("/Users/nuhabintayyash/GPcounts/paper_notebooks/tradeSeq/datasets_for_koen.rds")
+
+for(datasetIter in c(1:10)){
+
+  data <- dataAll[[datasetIter]]
+  counts <- t(data$counts)
+
+  # get milestones
+  gid <- data$prior_information$groups_id
+  gid <- gid[match(colnames(counts),gid$cell_id),]
+
+  pal <- wes_palette("Zissou1", 12, type = "continuous")
+  truePseudotime <- data$prior_information$timecourse_continuous
+  g <- Hmisc::cut2(truePseudotime,g=12)
+
+  # quantile normalization
+  normCounts <- FQnorm(counts)
+
+  ## dim red
+  pca <- prcomp(log1p(t(normCounts)), scale. = FALSE)
+  rd <- pca$x[,1:2]
+  #plot(rd, pch=16, asp = 1, col=pal[g])
+
+  library(princurve)
+  pcc <- principal_curve(rd, smoother="periodic_lowess")
+  #lines(x=pcc$s[order(pcc$lambda),1], y=pcc$s[order(pcc$lambda),2], col="red", lwd=2)
+
+
+  ggTraj <- ggplot(as.data.frame(rd), aes(x = PC1, y = PC2)) +
+    geom_point(col = pal[g], size=1) +
+    theme_classic() +
+    geom_path(x = pcc$s[order(pcc$lambda),1],
+              y = pcc$s[order(pcc$lambda),2])
+  assign(paste0("trajplot",datasetIter),ggTraj)
+}
+
+### fixed aspect ratio
+p1 <- plot_grid(trajplot1 + coord_fixed(), trajplot2 + coord_fixed(), trajplot3 + coord_fixed(), trajplot4 + coord_fixed(), trajplot5 + coord_fixed(),
+              bifplot1 + coord_fixed(), bifplot2 + coord_fixed(), bifplot3 + coord_fixed(), bifplot4 + coord_fixed(), bifplot5 + coord_fixed(),
+        nrow=2, ncol=5)#, rel_heights=c(0.8,1,0.8,1))
+pLeg1 <- plot_grid(p1, legend_all, rel_heights=c(1,0.15), nrow=2, ncol=1)
+pLeg1
+ggsave("/Users/nuhabintayyash/GPcounts/paper_notebooks/tradeSeq/individualPerformance_cyclic1To5_v2IncludingEdgeR.pdf", width = unit(15, "in"), height = unit(10, "in"), scale = .7)
+
+## fixed aspect ratio
+dev.new()
+p2 <- plot_grid(trajplot6 + coord_fixed(), trajplot7 + coord_fixed(), trajplot8 + coord_fixed(), trajplot9 + coord_fixed(), trajplot10 + coord_fixed(),
+                bifplot6 + coord_fixed(), bifplot7 + coord_fixed(), bifplot8 + coord_fixed(), bifplot9 + coord_fixed(), bifplot10 + coord_fixed(),
+        nrow=2, ncol=5, rel_heights=c(0.8,1))
+pLeg2 <- plot_grid(p2, legend_all, rel_heights=c(1,0.15), nrow=2, ncol=1)
+pLeg2
+ggsave("/Users/nuhabintayyash/GPcounts/paper_notebooks/tradeSeq/individualPerformance_cyclic6To10_v2IncludingEdgeR.pdf", width = unit(15, "in"), height = unit(10, "in"), scale = .7)
+
+# ### mean plot
+resList <- c()
+resList2 <- c()
+alphaSeq <- c(seq(1e-16,1e-9,length=100),seq(5e-9,1e-3,length=250),seq(5e-2,1,length=500))
+alphaSeq2 <- c(seq(-20,0,length=100),seq(0,3,length=250),seq(3,50,length=500))
+
+for(ii in 1:length(cobraFiles)){
+    cobra <- readRDS(cobraFiles[ii])
+    pvals <- pval(cobra)
+    colnames(pvals) <- gsub(colnames(pvals),pattern="tradeR",replacement="tradeSeq")
+    truths <- as.logical(truth(cobra)[,1])
+    # performance for all p-value based methods
+    hlp <- apply(pvals,2,function(x){
+      pOrder <- order(x,decreasing=FALSE)
+      padj <- p.adjust(x,"fdr")
+      df <- as.data.frame(t(sapply(alphaSeq, function(alpha){
+          R <- which(padj <= alpha)
+          fdr <- sum(!truths[R])/length(R) #false over rejected
+          tpr <- sum(truths[R])/sum(truths) #TP over all true
+          c(fdr=fdr, tpr=tpr, cutoff=alpha)
+      })))
+   
+    })
+    scores <- score(cobra) 
+    # performance for all score-value based methods
+    hlp2 <- apply(scores,2,function(x){
+      pOrder <- order(scores[,1],decreasing=FALSE)
+      df <- as.data.frame(t(sapply(alphaSeq2, function(alpha2){
+        R <- which(x >= alpha2)
+        fdr <- sum(!truths[R])/length(R) #false over rejected
+        tpr <- sum(truths[R])/sum(truths) #TP over all true
+        c(fdr=fdr, tpr=tpr, cutoff=alpha2)
+      })))
+     })
+    
+    # summarize
+    dfIter <- do.call(rbind,hlp)
+    dfIter2 <- do.call(rbind,hlp2)
+    dfIter$method=rep(colnames(pvals),each=length(alphaSeq))
+    dfIter2$method=rep(colnames(scores),each=length(alphaSeq))
+    dfIter$dataset <- ii
+    resList[[ii]] <- dfIter
+    dfIter2$dataset <- ii
+    resList2[[ii]] <- dfIter2
+}
+
+# #### across all datasets
+library(tidyverse)
+df <- as_tibble(do.call(rbind,resList))
+df <- df %>% group_by(method,cutoff) %>%
+        summarize(meanTPR=mean(tpr,na.rm=TRUE),
+                meanFDR=mean(fdr,na.rm=TRUE)) %>% arrange(method,cutoff)
+
+df2 <- as_tibble(do.call(rbind,resList2))
+df2 <- df2 %>% group_by(method,cutoff) %>%
+  summarize(meanTPR=mean(tpr,na.rm=TRUE),
+            meanFDR=mean(fdr,na.rm=TRUE)) %>% arrange(method,cutoff)
+
+pMeanAll <- ggplot(df, aes(x=meanFDR, y=meanTPR, col=method)) + geom_path(size = 1) +
+scale_x_continuous(limits = c(0, 1), breaks = c(0.01, 0.05, 0.1, 0.5, 1),
+                   minor_breaks = c(0:5) * .1) +
+scale_y_continuous(limits = c(0, 1)) +
+scale_color_manual(values = cols, breaks = names(cols)) +
+scale_linetype_manual(values = linetypes, breaks = names(linetypes)) + xlab("FDR") + ylab("TPR")
+
+pMeanLegAll <- plot_grid(pMeanAll, legend_all, rel_heights=c(1,0.15), nrow=2, ncol=1)
+pMeanLegAll
+
+pMeanAll2 <- ggplot(df2, aes(x=meanFDR, y=meanTPR, col=method)) + geom_path(size = 1) +
+scale_x_continuous(limits = c(0, 1), breaks = c(0.01, 0.05, 0.1, 0.5, 1),
+                   minor_breaks = c(0:5) * .1) +
+scale_y_continuous(limits = c(0, 1)) +
+scale_color_manual(values = cols, breaks = names(cols)) +
+scale_linetype_manual(values = linetypes, breaks = names(linetypes)) + xlab("FDR") + ylab("TPR")
+
+pMeanLegAll2 <- plot_grid(pMeanAll2, legend_all, rel_heights=c(1,0.15), nrow=2, ncol=1)
+pMeanLegAll2
+
+df_all <- rbind(df, df2)
+pMeanAll3 <- ggplot(df_all, aes(x=meanFDR, y=meanTPR, col=method)) + geom_path(size = 1) +
+  scale_x_continuous(limits = c(0, 1), breaks = c(0.01, 0.05, 0.1, 0.5, 1),
+                     minor_breaks = c(0:5) * .1) +
+  scale_y_continuous(limits = c(0, 1)) +
+  scale_color_manual(values = cols, breaks = names(cols)) +
+  scale_linetype_manual(values = linetypes, breaks = names(linetypes)) + xlab("FDR") + ylab("TPR")
+
+pMeanLegAll3 <- plot_grid(pMeanAll3, legend_all, rel_heights=c(1,0.15), nrow=2, ncol=1)
+pMeanLegAll3
+
+saveRDS(pMeanAll3, file="/Users/nuhabintayyash/GPcounts/paper_notebooks/tradeSeq/pMeanCycle_v2_GPcounts.rds")
